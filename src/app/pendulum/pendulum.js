c2424bd8c33a97a600e5a2b620310ee02be1d8ec
Paolo Cargnin <cargnin.paolo@gmail.com>
2015-08-26 02:19:58 +0200

    yeai! svg support!

--- src/app/pendulum/pendulum.js
+++ src/app/pendulum/pendulum.js
@@ -2,9 +2,9 @@
 
 angular.module('boom')
   .controller('PendulumController', function ($scope) {
-		$('<img/>').attr('src', 'assets/images/bg.jpg').load(function() {
-		 $(this).remove(); // prevent memory leaks as @benweet suggested
-		 $('body').css('background-image', 'url(assets/images/bg.jpg)');
-		});
+		// $('<img/>').attr('src', 'assets/images/bg.jpg').load(function() {
+		//  $(this).remove(); // prevent memory leaks as @benweet suggested
+		//  $('body').css('background-image', 'url(assets/images/bg.jpg)');
+		// });
   });
 
